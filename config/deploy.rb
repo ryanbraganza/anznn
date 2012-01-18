11dd2427d7eeaecd789efd7d4519a4ead79e3c8f
Ryan Braganza <ryan.braganza@gmail.com>
2012-01-18 13:04:27 +1100

    use postgres instead of sqlite3

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ require 'rvm/capistrano'
 set :application, 'anznn'
 set :stages, %w(qa staging production)
 set :default_stage, "qa"
-set :rpms, "openssl openssl-devel curl-devel httpd-devel apr-devel apr-util-devel zlib zlib-devel libxml2 libxml2-devel libxslt libxslt-devel libffi mod_ssl mod_xsendfile"
+set :rpms, "openssl openssl-devel curl-devel httpd-devel apr-devel apr-util-devel zlib zlib-devel libxml2 libxml2-devel libxslt libxslt-devel libffi mod_ssl mod_xsendfile postgresql84-server postgresql84 postgresql84-devel"
 set :shared_children, shared_children + %w(log_archive)
 set :shell, '/bin/bash'
 set :rvm_ruby_string, 'ruby-1.9.3-p0@anznn'
