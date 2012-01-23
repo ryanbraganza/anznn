41712720ea65b99d2ac7d7b9f1214c2c0658e39a
Alex Bradner <alex@bradner.net>
2012-01-23 17:06:53 +1100

    PERI-1 Fixing dependent: :destroy for relevant associations - Survey, Section, Question, Response

--- app/controllers/admin/admin_base_controller.rb
+++ app/controllers/admin/admin_base_controller.rb
@@ -2,7 +2,6 @@ class Admin::AdminBaseController < ApplicationController
 
   before_filter :authenticate_user!
   set_tab :admin
-  set_tab
 
   def index
     redirect_to admin_surveys_path
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,7 @@
 class Response < ActiveRecord::Base
   belongs_to :survey
   belongs_to :user
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates_presence_of :baby_code
   validates_presence_of :user
