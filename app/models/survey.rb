b20d0050676ebd35096d078af8366580b6b9d7fd
Ryan Braganza <ryan.braganza@gmail.com>
2012-01-23 14:37:19 +1100

    PERI-1 add ordered_questions to Survey
    add question retrieval by section.order, question.order to Survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,8 @@
 class Survey < ActiveRecord::Base
   has_many :responses
   has_many :sections
+
+  def ordered_questions
+    Question.joins(section: :survey).where(sections: {survey_id: id}).order('sections."order"', '"order"')
+  end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -5,4 +5,20 @@ describe Survey do
     it { should have_many :responses }
     it { should have_many :sections }
   end
+  describe :ordered_questions do
+    it "should retrieve questions ordered by section.order, question.order" do
+      survey = Factory(:survey)
+      s2 = Factory(:section, survey: survey, order: 2)
+      s1 = Factory(:section, survey: survey, order: 1)
+      q1b = Factory(:question, question: 'q1b', section: s1, order: 2)
+      q1a = Factory(:question, question: 'q1a', section: s1, order: 1)
+      q2a = Factory(:question, question: 'q2a', section: s2, order: 1)
+      q2b = Factory(:question, question: 'q2b', section: s2, order: 2)
+
+      expected = %w{q1a q1b q2a q2b}
+      actual = survey.ordered_questions.map{|q| q.question }
+
+      expected.should eq actual
+    end
+  end
 end
