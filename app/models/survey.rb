790484c4d8552c538b3ec3bd47b1070642cf5780
Alex Bradner <alex@bradner.net>
2012-01-23 15:30:44 +1100

    PERI-1 Small changes to survey model and starting a response

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,14 @@
 class Survey < ActiveRecord::Base
   has_many :responses
   has_many :sections
+  
+  #TODO - this will be used to select 'active' surveys if that is required.
+  # It is used in the 'new reponse' form, so currently it just returns all of them
+  scope :active, all
+
+
+  validates :name, presence: true
+
 
   def ordered_questions
     Question.joins(section: :survey).where(sections: {survey_id: id}).order('sections."order"', '"order"')
