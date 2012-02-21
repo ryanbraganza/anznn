0456bb7c2ae2d37bfe96ec9066c0920d08c31632
Ryan Braganza <ryan.braganza@gmail.com>
2012-02-21 12:52:29 +1100

    PERI-15 add cross validations to main survey

--- lib/tasks/sample_data_populator.rb
+++ lib/tasks/sample_data_populator.rb
@@ -15,31 +15,33 @@ def create_surveys
   Section.delete_all
   Question.delete_all
   QuestionOption.delete_all
-  create_survey("Main Survey", "main_survey_questions.csv", "main_survey_question_options.csv")
-  create_survey("Followup Survey", "followup_survey_questions.csv", "followup_survey_question_options.csv")
+  create_survey("Main Survey", "main_survey_questions.csv", "main_survey_question_options.csv", "main_cross_question_validations.csv")
+  create_survey("Followup Survey", "followup_survey_questions.csv", "followup_survey_question_options.csv", "followup_cross_question_validations.csv")
 
 end
 
-def create_survey(name, question_file, options_file)
+def create_survey(name, question_file, options_file, cross_question_validations_file)
   include CsvSurveyOperations
 
   survey = Survey.create!(name: name)
 
   questions = read_hashes_from_csv(Rails.root.join("lib/tasks", question_file))
   question_options = read_hashes_from_csv(Rails.root.join("lib/tasks", options_file))
+  cqv_hashes = read_hashes_from_csv(Rails.root.join("lib/tasks", cross_question_validations_file))
  
   import_questions(survey, questions)
   import_question_options(survey, question_options)
+  import_cross_question_validations(survey, cqv_hashes)
 end
 
 def create_test_users
   create_user(email: "sean@intersect.org.au", first_name: "Sean", last_name: "McCarthy")
-    create_user(email: "georgina@intersect.org.au", first_name: "Georgina", last_name: "Edwards")
-    create_user(email: "veronica@intersect.org.au", first_name: "Veronica", last_name: "Luke")
-    create_user(email: "alexb@intersect.org.au", first_name: "Alex", last_name: "Bradner")
-    create_user(email: "kali@intersect.org.au", first_name: "Kali", last_name: "Waterford")
-    create_user(email: "diego@intersect.org.au", first_name: "Diego", last_name: "Alonso de Marcos")
-    create_user(email: "shuqian@intersect.org.au", first_name: "Shuqian", last_name: "Hon")
+  create_user(email: "georgina@intersect.org.au", first_name: "Georgina", last_name: "Edwards")
+  create_user(email: "veronica@intersect.org.au", first_name: "Veronica", last_name: "Luke")
+  create_user(email: "alexb@intersect.org.au", first_name: "Alex", last_name: "Bradner")
+  create_user(email: "kali@intersect.org.au", first_name: "Kali", last_name: "Waterford")
+  create_user(email: "diego@intersect.org.au", first_name: "Diego", last_name: "Alonso de Marcos")
+  create_user(email: "shuqian@intersect.org.au", first_name: "Shuqian", last_name: "Hon")
   create_user(email: "dataprovider@intersect.org.au", first_name: "Data", last_name: "Provider")
   create_unapproved_user(email: "unapproved1@intersect.org.au", first_name: "Unapproved", last_name: "One")
   create_unapproved_user(email: "unapproved2@intersect.org.au", first_name: "Unapproved", last_name: "Two")
