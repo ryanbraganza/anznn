e7b50c0f11e9f99899c2c4a459d670cd223bf324
Georgina Edwards <georgina@intersect.org.au>
2012-03-02 15:38:41 +1100

    Fix constant naming in hospital

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,6 +1,6 @@
 class Hospital < ActiveRecord::Base
 
-  ValidStates = %w(NSW Vic Qld SA Tas WA NT ACT North\ Island South\ Island)
+  VALID_STATES = %w(NSW Vic Qld SA Tas WA NT ACT North\ Island South\ Island)
 
   has_many :users
   has_many :responses
@@ -9,28 +9,16 @@ class Hospital < ActiveRecord::Base
   validates_presence_of :state
   validates_presence_of :abbrev
   validates_inclusion_of :state,
-                         in: ValidStates,
-                         message: "Sate %{value} is not a valid Aus/NZ state or territory"
+                         in: VALID_STATES,
+                         message: "State %{value} is not a valid Aus/NZ state or territory"
 
   def self.all_states
-    ValidStates
+    VALID_STATES
   end
-
-  #define in_<state> 'scopes' for each state.
-  # All lowercase, spaces -> '_'
-  # Examples:
-  #  collect all nsw hospitals by using 'Hospital.in_nsw'
-  #  all north island hospitals - 'Hospital.in_north_island'
-  ValidStates.each do |state|
-    send :define_singleton_method, "in_#{state.gsub(' ', '_').downcase}" do
-      where(state: state)
-    end
-  end
-
 
   def self.hospitals_by_state
     groups = Hash.new
-    ValidStates.each do |state|
+    VALID_STATES.each do |state|
       hospitals = where(state: state).order(:name)
       mapped_hospitals = hospitals.collect do |h|
         [h.name, h.id]
