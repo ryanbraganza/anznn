a1a0c94cd21ace7692145c5111d59152237c708e
Alex Bradner <alex@bradner.net>
2012-03-02 11:38:22 +1100

    PERI-60 adding CQV test survey

--- lib/tasks/sample_data_populator.rb
+++ lib/tasks/sample_data_populator.rb
@@ -34,6 +34,7 @@ def create_surveys
   QuestionOption.delete_all
   create_survey_from_lib_tasks("Main Survey", "main_survey_questions.csv", "main_survey_question_options.csv", "main_cross_question_validations.csv")
   create_survey_from_lib_tasks("Followup Survey", "followup_survey_questions.csv", "followup_survey_question_options.csv", "followup_cross_question_validations.csv")
+  create_survey_from_lib_tasks("Test Survey", "test_survey_questions.csv", "test_survey_question_options.csv", "test_cross_question_validations.csv")
 end
 
 def create_survey_from_lib_tasks(name, question_file, options_file, cross_question_validations_file)
