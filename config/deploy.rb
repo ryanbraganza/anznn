5b0c3be8c174179c6b2f5444b42984ecf8e75c76
Diego Alonso <diego@intersect.org.au>
2012-06-26 12:01:06 +1000

    making whenever environment aware

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ require 'rvm/capistrano'
 require "delayed/recipes"
 require "bundler/capistrano"
 
+set :whenever_environment, defer { stage }
 set :whenever_command, "bundle exec whenever"
 require 'whenever/capistrano'
 
